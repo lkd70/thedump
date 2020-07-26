891a98da980fb4aaaf51ed5ddf357dac1daa54b0
LKD70 <lkdornan70@gmail.com>
2020-07-26 05:17:16 +0100

    updated example config

--- config.example.js
+++ config.example.js
@@ -1,30 +1,55 @@
 'use strict';
 
-const debug = false;
+const debug = true;
 
 const boards_config = {
-	b: { name: 'Random', code: 'b', path: 'com' },
-	gif: { name: 'Adult Gif', code: 'gif', path: 'sub' },
-	wsg: { name: 'Worksafe Gif', code: 'wsg', path: 'sub' },
+	b: {
+		name: 'Random',
+		code: 'b',
+		path: 'com'
+	},
+	gif: {
+		name: 'Adult Gif',
+		code: 'gif',
+		path: 'sub'
+	},
+	wsg: {
+		name: 'Worksafe Gif',
+		code: 'wsg',
+		path: 'sub'
+	},
 };
 
-const search = [
-	{
-		terms: [ 'ylyl' ],
-		boards: [ boards_config.b, boards_config.gif, boards_config.wsg ],
-		types: [ '.gif', '.png', '.jpg', '.webm' ],
-		chat_id: '-222222222222',
+const reddit_boards = {
+	gonewild: {
+		name: 'Reddit Gone Wild',
+		code: 'gonewild',
+	}
+}
+
+const reddit_search = [{
+	terms: ['[f]', '(f)'],
+	boards: [reddit_boards.gonewild],
+	types: ['.png', '.jpg'],
+	chat_id: [''],
+}];
+
+const search = [{
+		terms: ['ylyl'],
+		boards: [boards_config.b, boards_config.gif, boards_config.wsg],
+		types: ['.gif', '.png', '.jpg', '.webm'],
+		chat_id: '',
 	},
 	{
-		terms: [ 'rekt', 'gore' ],
-		boards: [ boards_config.b, boards_config.gif ],
-		types: [ '.gif', '.png', '.jpg', '.webm' ],
-		chat_id: '-333333333333',
+		terms: ['rekt', 'gore'],
+		boards: [boards_config.b, boards_config.gif],
+		types: ['.gif', '.png', '.jpg', '.webm'],
+		chat_id: '',
 	},
 ];
 
 const telegram = {
-	token: '1234567890:h9rdd4d4idn39dged8b3f04crc9u4fby3d',
+	token: '',
 	log_chat: null,
 };
 
@@ -32,4 +57,5 @@ module.exports = {
 	search,
 	telegram,
 	debug,
+	reddit_search,
 };
